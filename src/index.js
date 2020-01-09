5920977b02473904facb5acdbe44d149bf4ee2f9
henrych4 <singyinhenry@gmail.com>
2020-01-10 00:23:52 +0800

    Fix image not shown due to cache

--- src/index.js
+++ src/index.js
@@ -166,11 +166,11 @@ export default class extends PureComponent {
 
     // Load the image
     this.image = new Image();
-    this.image.src = this.props.imgSrc;
 
     // Draw the image once loaded
     this.image.onload = () =>
       drawImage({ ctx: this.ctx.grid, img: this.image });
+    this.image.src = this.props.imgSrc;
   };
 
   undo = () => {
